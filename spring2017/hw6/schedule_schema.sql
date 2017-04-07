b0317ab3bbc137b62ae7f119757517a3d0ebf23e
brandonbest13 <brandonbest13@gatech.edu>
2017-04-07 11:28:58 -0400

    update with lowercase table names
    lowercase table names

--- spring2017/hw6/schedule_schema.sql
+++ spring2017/hw6/schedule_schema.sql
@@ -1,4 +1,4 @@
-create table if not exists Course (
+create table if not exists course (
   CRN integer primary key,
   course_name text not null unique,
   hours int not null,
@@ -9,21 +9,21 @@ create table if not exists Course (
   class_req text
 );
 
-create table if not exists Student (
+create table if not exists student (
   username text primary key,
   name text not null,
   major text,
   year text
 );
 
-create table if not exists Registered (
+create table if not exists registered (
   course int,
   stud text,
-  FOREIGN KEY(course) REFERENCES Course(CRN),
-  FOREIGN KEY(stud) REFERENCES Student(username)
+  FOREIGN KEY(course) REFERENCES course(CRN),
+  FOREIGN KEY(stud) REFERENCES student(username)
 );
 
-create table if not exists Admin (
-  AdminID text primary key,
+create table if not exists admin (
+  adminID text primary key,
   password text not null
 );
