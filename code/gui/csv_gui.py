c417e5c89072c8d32ed3eaff5fd0d572bbd14c97
Chris Simpkins <chris.simpkins@gmail.com>
2017-04-10 12:33:26 -0400

    Add CSV and SQL GUI browser examples

---
+++ code/gui/csv_gui.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    qApp,
+    QAction,
+    QWidget,
+    QFileDialog,
+    QTableView
+)
+from PyQt5.QtGui import (
+    QStandardItemModel,
+    QStandardItem
+)
+from PyQt5.QtCore import (
+    Qt,
+    QAbstractTableModel,
+    QVariant
+)
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("CSV GUI")
+        self.create_menu_bar()
+
+    def create_menu_bar(self):
+        menu_bar = self.menuBar()
+        self.create_file_menu(menu_bar)
+
+    def create_file_menu(self, menu_bar):
+        exit = QAction("Exit", self)
+        exit.setShortcut("Ctrl+Q")
+        exit.setStatusTip("Exit CSV GUI")
+        exit.triggered.connect(qApp.quit)
+
+        open = QAction("&Open", self)
+        open.setShortcut("Ctrl+O")
+        open.setStatusTip("Open a CSV file")
+        open.triggered.connect(self.open_file)
+
+
+        file_menu = menu_bar.addMenu("&File")
+        file_menu.addAction(open)
+        file_menu.addAction(exit)
+
+    def open_file(self):
+        file_name, filter = \
+            QFileDialog.getOpenFileName(self, "Open file", ".",
+                                        "All files (*);;CSV Files (*.csv)")
+
+        with open(file_name) as fin:
+            csv_data = [row for row in csv.reader(fin)]
+        table_model = SimpleTableModel(csv_data[0], csv_data[1:])
+        table_view = QTableView()
+        table_view.setModel(table_model)
+        self.setCentralWidget(table_view)
+
+class SimpleTableModel(QAbstractTableModel):
+
+    def __init__(self, headers, rows):
+        QAbstractTableModel.__init__(self, None)
+        self.headers = headers
+        self.rows = rows
+
+    def rowCount(self, parent):
+        return len(self.rows)
+
+    def columnCount(self, parent):
+        return len(self.headers)
+
+    def data(self, index, role):
+        if (not index.isValid()) or (role != Qt.DisplayRole):
+            return QVariant()
+        else:
+            return QVariant(self.rows[index.row()][index.column()])
+
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole:
+            return QVariant()
+        elif orientation == Qt.Vertical:
+            return section + 1
+        else:
+            return self.headers[section]
+
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
